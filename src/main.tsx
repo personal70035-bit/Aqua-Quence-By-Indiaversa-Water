00f9fe7a0ff78a0d50423b18f0f77f4ee3cbe397
personal70035-bit <personal.70035@gmail.com>
2026-03-28 17:59:47 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import { callAPI } from "./api";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
 );
+
+callAPI().then(data => {
+  console.log("API RESPONSE:", data);
+});
