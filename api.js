88b55bec8d972ed8c4158b5d10b7f6a7326bf13b
personal70035-bit <personal.70035@gmail.com>
2026-03-28 18:32:58 +0530

    Update api.js

--- api.js
+++ api.js
@@ -1,45 +1,13 @@
 export default {
-  async fetch(request, env) {
-
-    if (request.method === "OPTIONS") {
-      return new Response(null, {
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-          "Access-Control-Allow-Headers": "*"
-        }
-      });
-    }
-
-    try {
-      const response = await fetch("https://api.openai.com/v1/responses", {
-        method: "POST",
-        headers: {
-          "Authorization": `Bearer ${env.OPENAI_API_KEY}`,
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-          model: "gpt-4.1",
-          input: "Hello"
-        })
-      });
-
-      const data = await response.text();
-
-      return new Response(data, {
-        headers: {
-          "Content-Type": "application/json",
-          "Access-Control-Allow-Origin": "*"
-        }
-      });
-
-    } catch (err) {
-      return new Response(JSON.stringify({ error: err.message }), {
-        headers: {
-          "Content-Type": "application/json",
-          "Access-Control-Allow-Origin": "*"
-        }
-      });
-    }
+  async fetch(request) {
+    return new Response(JSON.stringify({
+      status: "WORKING",
+      message: "Worker is live"
+    }), {
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*"
+      }
+    });
   }
 };
